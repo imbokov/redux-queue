c3fd315ff8b371b16228b351fd3b349cdf84d35b
imbokov <tapycx@gmail.com>
2019-12-09 15:40:14 +0700

    api setup

--- app/src/store.js
+++ app/src/store.js
@@ -2,8 +2,8 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
+import apiFetch from "helpers/apiFetch";
 import reducer from "modules";
-import apiFetch from "modules/request/helpers/apiFetch";
 
 const store = createStore(
   reducer,
